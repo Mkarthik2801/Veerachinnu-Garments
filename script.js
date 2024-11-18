b3ee4139fefcfd55e485803c415d1b0676b2f6a2
Karthikpalraj M <mkarthikpalraj4@gmail.com>
2024-11-18 16:19:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle mobile menu visibility
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
